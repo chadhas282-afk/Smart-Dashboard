d39105d2717476d3056b4aa654e02bb72c077c36
chadhas282-afk <chadhas282@gmail.com>
2026-02-04 14:33:12 +0530

    logic added

---
+++ script.js
@@ -0,0 +1,5 @@
+let inp = document.getElementById("eventInput").value
+let date = document.getElementById("eventDate").value
+let cate = document.getElementById("eventCategory").value
+let des = document.getElementById("description").value
+let eve = document.getElementById("addEventButton")
