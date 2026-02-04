2bab08c34677cd01cd248705eda6b0abbf09e5af
chadhas282-afk <chadhas282@gmail.com>
2026-02-04 14:37:21 +0530

    logic linked with card

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let inp = document.getElementById("eventInput").value
-let date = document.getElementById("eventDate").value
-let cate = document.getElementById("eventCategory").value
-let des = document.getElementById("description").value
-let eve = document.getElementById("addEventButton")
+let inp = document.getElementById("eventInput");
+let date = document.getElementById("eventDate");
+let cate = document.getElementById("eventCategory");
+let des = document.getElementById("description");
+let eve = document.getElementById("addEventButton");
+
+let card = `<div class="grid">
+                <p class="name">${inp.value}</p>
+                <p class="date">🗓️${date.value}</p>
+                <label class="cat">${cate.value}</label>
+                <p class="des">${des.value}</p>
+            </div>`;
